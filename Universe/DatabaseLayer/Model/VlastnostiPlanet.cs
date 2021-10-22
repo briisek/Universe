199fd7e440c57ff1dc3c3094de86b75fb676f315
Boris Zálešák <boris.zalesak@gmail.com>
2021-10-22 08:28:47 +0200

    adding files

---
+++ Universe/DatabaseLayer/Model/VlastnostiPlanet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Universe.DatabaseLayer.Model
+{
+    public partial class VlastnostiPlanet
+    {
+        public int PlanetaId { get; set; }
+        public int VlastnostId { get; set; }
+
+        public virtual Planetum Planeta { get; set; }
+        public virtual Vlastnost Vlastnost { get; set; }
+    }
+}
