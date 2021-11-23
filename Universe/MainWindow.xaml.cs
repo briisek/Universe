c2b75dd32dc30caec303fe7fbed51d6761b50d6c
briisek <boris.zalesak@gmail.com>
2021-11-23 09:03:40 +0100

    Nejake dalsi upravy

--- Universe/MainWindow.xaml.cs
+++ Universe/MainWindow.xaml.cs
@@ -58,85 +58,134 @@ namespace Universe
             base.OnClosing(e);
         }
 
-        private void galaxiesDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        #region Planets Properties
+
+        private void BtnAddProperty_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtPropertyName.Text))
+            {
+                return;
+            }
+
+            Vlastnost vlastnost = new Vlastnost() { Nazev = txtPropertyName.Text, Id = _context.Vlastnosts.Max(x => x.Id) + 1 };
+            _ = _context.Vlastnosts.Add(vlastnost);
+            _ = _context.SaveChanges();
+            PlanetsDataGrid_SelectionChanged(planetsDataGrid, null);
+        }
+
+        private void BtnRemoveProperties_Click(object sender, RoutedEventArgs e)
+        {
+            RemoveProperties();
+        }
+
+        private void DataGrid_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Delete)
+            {
+                RemoveProperties();
+            }
+        }
+
+        private void RemoveProperties()
+        {
+            List<Vlastnost> vlastnosts = new();
+
+            foreach (Vlastnost vlastnost in propertiesDataGrid.SelectedItems)
+            {
+                _context.VlastnostiPlanets.RemoveRange(_context.VlastnostiPlanets.Where(x => x.VlastnostId == vlastnost.Id));
+                vlastnosts.Add(vlastnost);
+            }
+
+            _context.RemoveRange(vlastnosts);
+            _context.SaveChanges();
+            RefreshDataGrids();
+            PlanetsDataGrid_SelectionChanged(planetsDataGrid, null);
+        }
+
+        private void RefreshDataGrids()
+        {
+            propertiesDataGrid.Items.Refresh();
+            planetsDataGrid.Items.Refresh();
+        }
+
+        #endregion
+
+        #region Galaxies and Planets
+
+        /// <summary>
+        /// Load planets from mulitple galaxies.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GalaxiesDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             ICollection<Planetum> planetum = new ObservableCollection<Planetum>();
 
-            foreach (var galaxie in galaxiesDataGrid.SelectedItems)
+            foreach (Galaxie galaxie in galaxiesDataGrid.SelectedItems)
             {
-                foreach (var planeta in ((Universe.DatabaseLayer.Model.Galaxie)galaxie).Planeta)
+                foreach (Planetum planeta in ((Universe.DatabaseLayer.Model.Galaxie)galaxie).Planeta)
                 {
                     planetum.Add(planeta);
                 }
             }
             planetsDataGrid.ItemsSource = planetum;
         }
-
-        private void planetsDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        
+        /// <summary>
+        /// Create checkboxes representing planets properties.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PlanetsDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            st1.Children.Clear();
+            stCheckBoxes.Children.Clear();
 
-            if (((DataGrid)sender).SelectedItem == null) return;
-
-
-            int planeta_id = ((Planetum)((DataGrid)sender).SelectedItem).Id;
-
-            foreach (var item in _context.Vlastnosts.ToList())
+            if (((DataGrid)sender).SelectedItem == null)
             {
-                bool result = _context.VlastnostiPlanets.Any(x => x.VlastnostId == item.Id && x.PlanetaId == planeta_id);
-                CheckBox chk = new() { Content = item.Nazev, IsChecked = result };
-                chk.Checked += this.CheckBoxOnChecked;
-                chk.Unchecked += this.CheckBoxOnUnchecked;
-                st1.Children.Add(chk);
+                return;
             }
 
+            int planetaId = ((Planetum)((DataGrid)sender).SelectedItem).Id;
+
+            foreach (Vlastnost item in _context.Vlastnosts.ToList())
+            {
+                bool result = _context.VlastnostiPlanets.Any(x => x.VlastnostId == item.Id && x.PlanetaId == planetaId);
+                CheckBox chk = new() { Content = item.Nazev, IsChecked = result, Margin = new Thickness(0, 0, 10, 0) };
+                chk.Checked += CheckBoxOnChecked;
+                chk.Unchecked += CheckBoxOnUnchecked;
+                _ = stCheckBoxes.Children.Add(chk);
+            }
         }
 
         private void CheckBoxOnChecked(object sender, RoutedEventArgs routedEventArgs)
         {
-            if (sender is CheckBox checkBox)
-            {
-                int planeta_id = ((Planetum)planetsDataGrid.SelectedItem).Id;
-                string vlastnost_nazev = checkBox.Content.ToString();
+            int planetaId = ((Planetum)planetsDataGrid.SelectedItem).Id;
+            string vlastnostNazev = ((CheckBox)sender).Content.ToString();
 
-                Vlastnost vlastnost = _context.Vlastnosts.SingleOrDefault(x => x.Nazev == vlastnost_nazev);
-                int vlastnost_id = _context.Vlastnosts.Where(x => x.Nazev == vlastnost_nazev).AsNoTracking().FirstOrDefault().Id;
+            Vlastnost vlastnost = _context.Vlastnosts.SingleOrDefault(x => x.Nazev == vlastnostNazev);
+            int vlastnostId = _context.Vlastnosts.Where(x => x.Nazev == vlastnostNazev).FirstOrDefault().Id;
 
-                VlastnostiPlanet vlastnostiPlanet = new VlastnostiPlanet() { PlanetaId = planeta_id, VlastnostId = vlastnost_id };
-                _context.VlastnostiPlanets.Add(vlastnostiPlanet);
-                int result = _context.SaveChanges();
-                planetsDataGrid.Items.Refresh();
-            }
+            VlastnostiPlanet vlastnostiPlanet = new() { PlanetaId = planetaId, VlastnostId = vlastnostId };
+            _ = _context.VlastnostiPlanets.Add(vlastnostiPlanet);
+            _ = _context.SaveChanges();
+            RefreshDataGrids();
         }
 
         private void CheckBoxOnUnchecked(object sender, RoutedEventArgs routedEventArgs)
         {
-            if (sender is CheckBox checkBox)
+            int planetaId = ((Planetum)planetsDataGrid.SelectedItem).Id;
+            int vlastnostId = _context.Vlastnosts.Where(x => x.Nazev == ((CheckBox)sender).Content.ToString()).FirstOrDefault().Id;
+
+            VlastnostiPlanet vlastnostiPlanet = _context.VlastnostiPlanets.FirstOrDefault(x => x.VlastnostId == vlastnostId && x.PlanetaId == planetaId);
+
+            if (vlastnostiPlanet != null)
             {
-                int planeta_id = ((Planetum)planetsDataGrid.SelectedItem).Id;
-                int vlastnost_id = _context.Vlastnosts.Where(x => x.Nazev == checkBox.Content.ToString()).AsNoTracking().FirstOrDefault().Id;
-
-                VlastnostiPlanet vlastnostiPlanet = _context.VlastnostiPlanets.AsNoTracking().FirstOrDefault(x => x.VlastnostId == vlastnost_id && x.PlanetaId == planeta_id);
-
-                if (vlastnostiPlanet != null)
-                {
-                    _context.VlastnostiPlanets.Remove(vlastnostiPlanet);
-                }
-                int result = _context.SaveChanges();
-                planetsDataGrid.Items.Refresh();
+                _ = _context.VlastnostiPlanets.Remove(vlastnostiPlanet);
             }
+            _ = _context.SaveChanges();
+            RefreshDataGrids();
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            if (string.IsNullOrEmpty(txtPropertyName.Text)) return;
-
-            int maxId = _context.Vlastnosts.Max(x => x.Id);
-            Vlastnost vlastnost = new Vlastnost() { Nazev = txtPropertyName.Text, Id = maxId + 1  };
-            _context.Vlastnosts.Add(vlastnost);
-            _context.SaveChanges();
-
-        }
+        #endregion
     }
 }
 
