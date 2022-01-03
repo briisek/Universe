3f65a9a9f217e8232d918ecb63511b4cac688d69
Boris <boris.zalesak@gmail.com>
2022-01-03 17:21:58 +0100

    Program file.

---
+++ Universe.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Universe.Entities;
+
+namespace Universe.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UniverseContext universeContext = new UniverseContext();
+            universeContext.ListPlanetProperties();
+        }
+    }
+}
