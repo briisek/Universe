770da1b85acb47e47bca79a9411c31acbed2460f
Boris <boris.zalesak@gmail.com>
2022-01-03 17:20:52 +0100

    Vlastnost.

---
+++ Universe.Entities/Vlastnost.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+using LinqToDB.DataProvider.SqlServer;
+using LinqToDB.Mapping;
+
+namespace Universe.Entities
+{
+    [Table("Vlastnost")]
+    public class Vlastnost
+    {
+        [PrimaryKey, Identity]
+        public int Id { get; set; }
+        
+        [Column, Nullable]
+        public string Nazev { get; set; }
+    }
+}
