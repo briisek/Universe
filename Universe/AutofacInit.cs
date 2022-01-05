3a85e17b3e52e80847d47b5a6e8ba4b914481c3f
Boris <boris.zalesak@gmail.com>
2022-01-05 15:27:24 +0100

    hgjghjgh

--- Universe/AutofacInit.cs
+++ Universe/AutofacInit.cs
@@ -4,23 +4,26 @@ using Nielsen.Admosphere.Fw2.DataAccess.Linq2db.Autofac;
 using Nielsen.Admosphere.Fw2.DataAccess.Linq2db.Dao;
 using Universe.Entities;
 
-namespace Universe;
-
-public class AutofacInit
+namespace Universe
 {
-    public static ILifetimeScope Scope { get; private set; }
 
-    public void Init()
+    public class AutofacInit
     {
-        var builder = new ContainerBuilder();
+        public static ILifetimeScope Scope { get; private set; }
 
-        builder.RegisterType<EntitiesSource>().As<IEntitiesSource>().SingleInstance();
-        
-        DependencyDaoModule dependencyDaoModule = new DependencyDaoModule(new[] { Assembly.GetAssembly(typeof(Galaxie)) },
-            new DaoSourceConnectionStringSettings("vesmir", UniverseContext.ConnectionString));
+        public void Init()
+        {
+            var builder = new ContainerBuilder();
 
-        builder.RegisterModule(dependencyDaoModule);
-        
-        Scope = builder.Build();
+            builder.RegisterType<EntitiesSource>().As<IEntitiesSource>().SingleInstance();
+
+            DependencyDaoModule dependencyDaoModule = new DependencyDaoModule(
+                new[] {Assembly.GetAssembly(typeof(Galaxie))},
+                new DaoSourceConnectionStringSettings("vesmir", UniverseContext.ConnectionString));
+
+            builder.RegisterModule(dependencyDaoModule);
+
+            Scope = builder.Build();
+        }
     }
 }
